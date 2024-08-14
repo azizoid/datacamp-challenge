25baacfe5148c84cc7366f57ad43429908aa14f0
azizoid <shaqhuseynov@gmail.com>
2024-08-14 14:51:01 +0200

    style: update previewpage styles

--- src/pages/PreviewPage/PreviewPage.tsx
+++ src/pages/PreviewPage/PreviewPage.tsx
@@ -1,10 +1,7 @@
 export const PreviewPage = () => {
   const html = localStorage.getItem('markdown:markedHtml')
 
-  return <div className="mt-10 w-full">
-    <div
-      className="w-1/2 h-64 border p-2 overflow-auto"
-      {...(html && html.length > 0 ? { dangerouslySetInnerHTML: { __html: html } } : null)}
-    />
-  </div >
+  return <div className="w-full h-full"
+    {...(html && html.length > 0 ? { dangerouslySetInnerHTML: { __html: html } } : null)}
+  />
 }
