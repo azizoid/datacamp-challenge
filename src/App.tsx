adf76437afeaf147fb465bcd5a0b8959ff10b3b7
azizoid <shaqhuseynov@gmail.com>
2024-08-14 14:24:45 +0200

    feature: add rotuing to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
-import { useState } from 'react';
-import { Header } from './components/Header';
-import { parseMarkdown } from './utilities/markdownParser';
+import {
+  RouterProvider,
+} from "react-router-dom";
 
+import { Header } from './components/Header';
+import { router } from "./routes";
 
 export const App = () => {
-  const [markdown, setMarkdown] = useState('');
-  const [html, setHtml] = useState('');
-
-  const handleRender = async () => {
-    const markedResult = await parseMarkdown(markdown)
-    setHtml(markedResult);
-  };
-
   return (
     <div className="flex flex-col items-center">
       <Header />
 
-      <div className="flex w-3/4 mb-4 mt-10">
-        <textarea
-          className="w-1/2 h-64 border p-2 mr-2"
-          placeholder="Enter your markdown here..."
-          value={markdown}
-          onChange={(e) => setMarkdown(e.target.value)}
-        />
-        <div
-          className="w-1/2 h-64 border p-2 overflow-auto"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-      <button
-        className="px-4 py-2 bg-green-500 text-white rounded"
-        onClick={handleRender}
-      >
-        RENDER
-      </button>
+      <RouterProvider router={router} />
     </div>
-  );
+  )
 }
